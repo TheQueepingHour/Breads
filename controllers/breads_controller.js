ed7192c64fdf0042f0bf66b7023eab6f767f31cc
Josh Rivera <beps@Mactop.local>
2024-01-25 14:14:05 -0600

    Added new page for creating new bread, now working on form

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -11,8 +11,32 @@ breads.get('/', (req, res) => {
     // res.send(Bread)
 })
 
+//New
+breads.get('/new', (req, res) => {
+    res.render('new')
+})
+
+//Show
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
+    if (Bread[req.params.arrayIndex]) {
+        res.render('Show', {
+            bread: Bread[req.params.arrayIndex]
+        })
+    } else {
+        res.render('404 Error: Page not found')
+    }
+    // res.send(Bread[req.params.arrayIndex])
+})
+
+//Create
+breads.post('/', (req, res) => {
+    if(req.body.hasGluten === 'on'){
+        req.body.hasGluten = 'true'
+    } else {
+        req.body.hasGluten = 'false'
+    }
+    Bread.push(req.body)
+    res.redirect('/breads')
 })
 
 module.exports = breads
--- server.js
+++ server.js
@@ -4,10 +4,6 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Welcome to an awesome app about breads.')
-})
-
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
@@ -15,6 +11,16 @@ app.use('/breads', breadsController)
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+    res.send('Welcome to an awesome app about breads.')
+})
+
+app.get('*', (req, res) => {
+    res.send('Error 404: Page not found.')
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`)
